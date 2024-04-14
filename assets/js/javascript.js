f5d9d65c0993ed428f8b338df807170d3ee68052
NotSnowWhite <gflatch@att.net>
2024-04-14 17:17:39 -0500

    added fetch request and promises as well as catch for errors. updated variables for readability and ceated an object for lat and lon to use for later.

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,15 +3,20 @@ const form = document.querySelector('.formSubmit');
 const results = document.getElementsByClassName('results');
 const button = document.querySelector('button');
 const fiveDayForecastAPI = 'c7fc1fd28ddf8fba0642efaab611afd3';
-let lat;
-let lon;
+let lat = 32.3633005;
+let lon = -96.8751270;
+let coordinates = {lat, lon};
 const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${fiveDayForecastAPI}`;
 
 
-fetch {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+fetch(url)
+
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+    console.error(error);
+})

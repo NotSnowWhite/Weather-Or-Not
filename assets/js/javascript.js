9d5936dc7ae4134edad340ed60919fe3b030df48
NotSnowWhite <gflatch@att.net>
2024-04-14 19:23:37 -0500

    added function to request city weather data from geocoding api and created form handler with submit event handler and called the city function inside with a console log

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,22 +1,43 @@
+const fiveDayForecastAPI = 'c7fc1fd28ddf8fba0642efaab611afd3';
 const userSearch = document.getElementById('search');
 const form = document.querySelector('.formSubmit');
 const results = document.getElementsByClassName('results');
 const button = document.querySelector('button');
-const fiveDayForecastAPI = 'c7fc1fd28ddf8fba0642efaab611afd3';
-let lat = 32.3633005;
-let lon = -96.8751270;
-let coordinates = {lat, lon};
+let lat;
+let lon;
+let coordinates = { lat, lon };
 const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${fiveDayForecastAPI}`;
 
 
-fetch(url)
+// fetch(url)
 
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-    console.error(error);
+//     .then(response => {
+//         return response.json();
+//     })
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     })
+
+
+    // function that requests response from geocoding api to be called upon in form handler
+function cityLocation(city) {
+fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${fiveDayForecastAPI}`)
+.then(response => {
+    return response.json();
+})
+.then(data => {
+    console.log(data);
+})
+.catch(error => {
+    console.log(error);
+})
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const city = userSearch.value;
+    cityLocation(city);
 })

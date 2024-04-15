a38e30c8201e4b4a8c8f5817637d74df9d5ebcfa
NotSnowWhite <gflatch@att.net>
2024-04-14 23:09:00 -0500

    added line to clear results upon new search. created variable to create h2 with state and city names. and appended

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,11 +1,14 @@
 const fiveDayForecastAPI = 'c7fc1fd28ddf8fba0642efaab611afd3';
 const userSearch = document.getElementById('search');
 const form = document.querySelector('.formSubmit');
-const results = document.getElementsByClassName('results');
+const results = document.querySelector('div');
 const button = document.querySelector('button');
 
 // function gets city lat.lon from geocoding api and passes it in variables to weather api to request data
 function cityLocation(city) {
+    // clear the results at every new search
+    results.textContent = "";
+
     fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${fiveDayForecastAPI}`)
         .then(response => {
             return response.json();
@@ -14,10 +17,16 @@ function cityLocation(city) {
             console.log(data);
             // loop through the lon and lat for each city with the same name
             data.forEach(location => {
-
+                const container = document.createElement('div');
                 const lat = location.lat;
-                const lon = location.lon
-                // console.log(lat, lon);
+                const lon = location.lon;
+
+                const stateCity = document.createElement('h2');
+                stateCity.textContent = location.name + ', ' + location.state;
+                console.log(stateCity);
+
+                container.appendChild(stateCity);
+                results.append(container);
 
                 // specify url for lat.lon inside () after lat.lon variables are created and fetch data 
                 const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${fiveDayForecastAPI}`;

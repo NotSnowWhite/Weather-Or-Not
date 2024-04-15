a4965389cf6be5f816730307d7ad0521468715dd
NotSnowWhite <gflatch@att.net>
2024-04-15 18:14:58 -0500

    added variables for sunrise and set times and appended to details html.

--- assets/js/details.js
+++ assets/js/details.js
@@ -8,7 +8,7 @@ const display = document.getElementById('display');
 const date = document.createElement('p');
 date.textContent = dayjs().format('MM/DD/YYYY');
 display.appendChild(date);
-
+// new fetch to get and append data to the page
 fetch(url)
     .then(response => {
         return response.json();
@@ -17,11 +17,17 @@ fetch(url)
         console.log(data);
         const container = document.createElement('div');
         const sunrise = document.createElement('p');
-        const sunriseDate = new Date(data.city.sunrise * 1000);
-        const adjustedSunrise = new Date(sunriseDate.getTime() + data.timezone * 1000);
-        const sunriseComplete = adjustedSunrise.toLocaleDateString();
-        sunrise.textContent = sunriseComplete;
+        const sunriseUnix = data.city.sunrise;
+        const sunriseTime = dayjs.unix(sunriseUnix).format('hh:mm A');
+        sunrise.textContent = `Sunrise: ${sunriseTime}`;
+
+        const sunset = document.createElement('p');
+        const sunsetUnix = data.city.sunset;
+        const sunsetTime = dayjs.unix(sunsetUnix).format('hh:mm A');
+        sunset.textContent = `Sunset: ${sunsetTime}`;
+
         container.appendChild(sunrise);
+        container.appendChild(sunset);
 
         display.append(container);
     })

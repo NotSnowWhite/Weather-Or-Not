515d2bbbf4070b8695aee0accbfb4c862a7c9c51
NotSnowWhite <gflatch@att.net>
2024-04-15 12:17:00 -0500

    updated geocoding url to read citystate parameter in input and read any country data.

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -5,77 +5,73 @@ const results = document.querySelector('div');
 const button = document.querySelector('button');
 const stateMenu = document.getElementById('stateMenu');
 
-// dropdown for state search
-const stateInfo = [{code: 'AL', name: 'Alabama'},
-{code: 'AK', name:'Alaska'},
-{code: 'AZ', name:'Arizona'},
-{code: 'AR', name:'Arkansas'},
-{code: 'CA', name:'California'},
-{code: 'CO', name:'Colorado'},
-{code: 'CT', name:'Connecticut'},
-{code: 'DE', name:'Delaware'},
-{code: 'FL', name:'Florida'},
-{code: 'GA', name:'Georgia'},
-{code: 'HI', name:'Hawaii'},
-{code: 'ID', name:'Idaho'},
-{code: 'IL', name:'Illinois'},
-{code: 'IN', name:'Indiana'},
-{code: 'IA', name:'Iowa'},
-{code: 'KS', name:'Kansas'},
-{code: 'KY', name:'Kentucky'},
-{code: 'LA', name:'Louisiana'},
-{code: 'ME', name:'Maine'},
-{code: 'MD', name:'Maryland'},
-{code: 'MA', name:'Massachusetts'},
-{code: 'MI', name:'Michigan'},
-{code: 'MN', name:'Minnesota'},
-{code: 'MS', name:'Mississippi'},
-{code: 'MO', name:'Missouri'},
-{code: 'MT', name:'Montana'},
-{code: 'NE', name:'Nebraska'},
-{code: 'NV', name:'Nevada'},
-{code: 'NH', name:'New Hampshire'},
-{code: 'NJ', name:'New Jersey'},
-{code: 'NM', name:'New Mexico'},
-{code: 'NY', name:'New York'},
-{code: 'NC', name:'North Carolina'},
-{code: 'ND', name:'North Dakota'},
-{code: 'OH', name:'Ohio'},
-{code: 'OK', name:'Oklahoma'},
-{code: 'OR', name:'Oregon'},
-{code: 'PA', name:'Pennsylvania'},
-{code: 'PR', name:'Puerto Rico'},
-{code: 'RI', name:'Rhode Island'},
-{code: 'SC', name:'South Carolina'},
-{code: 'SD', name:'South Dakota'},
-{code: 'TN', name:'Tennessee'},
-{code: 'TX', name:'Texas'},
-{code: 'UT', name:'Utah'},
-{code: 'VT', name:'Vermont'},
-{code: 'VA', name:'Virginia'},
-{code: 'WA', name:'Washington'},
-{code: 'WV', name:'West Virginia'},
-{code: 'WI', name:'Wisconsin'},
-{code: 'WY', name:'Wyoming'},];
+// // dropdown for state search
+// // const stateInfo = [{code: 'AL', name: 'Alabama'},
+// // {code: 'AK', name:'Alaska'},
+// // {code: 'AZ', name:'Arizona'},
+// // {code: 'AR', name:'Arkansas'},
+// // {code: 'CA', name:'California'},
+// // {code: 'CO', name:'Colorado'},
+// // {code: 'CT', name:'Connecticut'},
+// // {code: 'DE', name:'Delaware'},
+// // {code: 'FL', name:'Florida'},
+// // {code: 'GA', name:'Georgia'},
+// // {code: 'HI', name:'Hawaii'},
+// // {code: 'ID', name:'Idaho'},
+// // {code: 'IL', name:'Illinois'},
+// // {code: 'IN', name:'Indiana'},
+// // {code: 'IA', name:'Iowa'},
+// // {code: 'KS', name:'Kansas'},
+// // {code: 'KY', name:'Kentucky'},
+// // {code: 'LA', name:'Louisiana'},
+// // {code: 'ME', name:'Maine'},
+// // {code: 'MD', name:'Maryland'},
+// // {code: 'MA', name:'Massachusetts'},
+// // {code: 'MI', name:'Michigan'},
+// // {code: 'MN', name:'Minnesota'},
+// // {code: 'MS', name:'Mississippi'},
+// // {code: 'MO', name:'Missouri'},
+// // {code: 'MT', name:'Montana'},
+// // {code: 'NE', name:'Nebraska'},
+// // {code: 'NV', name:'Nevada'},
+// // {code: 'NH', name:'New Hampshire'},
+// // {code: 'NJ', name:'New Jersey'},
+// // {code: 'NM', name:'New Mexico'},
+// // {code: 'NY', name:'New York'},
+// // {code: 'NC', name:'North Carolina'},
+// // {code: 'ND', name:'North Dakota'},
+// // {code: 'OH', name:'Ohio'},
+// // {code: 'OK', name:'Oklahoma'},
+// // {code: 'OR', name:'Oregon'},
+// // {code: 'PA', name:'Pennsylvania'},
+// // {code: 'PR', name:'Puerto Rico'},
+// // {code: 'RI', name:'Rhode Island'},
+// // {code: 'SC', name:'South Carolina'},
+// // {code: 'SD', name:'South Dakota'},
+// // {code: 'TN', name:'Tennessee'},
+// // {code: 'TX', name:'Texas'},
+// // {code: 'UT', name:'Utah'},
+// // {code: 'VT', name:'Vermont'},
+// // {code: 'VA', name:'Virginia'},
+// // {code: 'WA', name:'Washington'},
+// // {code: 'WV', name:'West Virginia'},
+// // {code: 'WI', name:'Wisconsin'},
+// // {code: 'WY', name:'Wyoming'},];
 
 
-stateInfo.forEach(state => {
-  const option = document.createElement('option');
-  option.value = state.code;
-  option.textContent = state.name;
-  stateMenu.appendChild(option);
-});
+// // stateInfo.forEach(state => {
+// //   const option = document.createElement('option');
+// //   option.value = state.code;
+// //   option.textContent = state.name;
+// //   stateMenu.appendChild(option);
+// // });
 
-stateMenu.addEventListener('change', () => {
-    const selectedState = stateMenu.value;
-    console.log(selectedState);
-});
 // function gets city lat.lon from geocoding api and passes it in variables to weather api to request data
-function cityLocation(city, state) {
+function cityLocation(cityState) {
     // clear the results at every new search
     results.textContent = "";
 
-    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city},${state},us&limit=5&appid=${fiveDayForecastAPI}`)
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityState},&limit=5&appid=${fiveDayForecastAPI}`)
         .then(response => {
             return response.json();
         })
@@ -119,13 +115,13 @@ function cityLocation(city, state) {
 // on form submit, call the function to obtain search data
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const city = userSearch.value;
-    const stateName = stateMenu.value;
-    cityLocation(city, stateName);
+    const cityState = userSearch.value;
+    // // const stateName = stateMenu.value;
+    cityLocation(cityState);
 })
 
 button.addEventListener('click', () => {
-    const city = userSearch.value;
-    const stateName = stateMenu.value;
-    cityLocation(city, stateName);
+    const cityState = userSearch.value;
+    // // const stateName = stateMenu.value;
+    cityLocation(cityState);
 })

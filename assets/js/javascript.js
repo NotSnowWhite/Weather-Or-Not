3aa258aba7ce53f43ebd921c4314b8c50ef8ff26
NotSnowWhite <gflatch@att.net>
2024-04-14 21:41:46 -0500

    commented my button back in both in html and js. added event listener to button to perform the same function on click

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,41 +3,53 @@ const userSearch = document.getElementById('search');
 const form = document.querySelector('.formSubmit');
 const results = document.getElementsByClassName('results');
 const button = document.querySelector('button');
-let lat;
-let lon;
-let coordinates = { lat, lon };
-const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${fiveDayForecastAPI}`;
 
-
-// fetch(url)
-
-//     .then(response => {
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch(error => {
-//         console.error(error);
-//     })
-
-
-    // function that requests response from geocoding api to be called upon in form handler
+// function gets city lat.lon from geocoding api and passes it in variables to weather api to request data
 function cityLocation(city) {
-fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${fiveDayForecastAPI}`)
-.then(response => {
-    return response.json();
-})
-.then(data => {
-    console.log(data);
-})
-.catch(error => {
-    console.log(error);
-})
-}
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${fiveDayForecastAPI}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            // loop through the lon and lat for each city with the same name
+            data.forEach(location => {
 
+                const lat = location.lat;
+                const lon = location.lon
+                // console.log(lat, lon);
+
+                // specify url for lat.lon inside () after lat.lon variables are created and fetch data 
+                const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${fiveDayForecastAPI}`;
+
+                fetch(url)
+
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(weather => {
+                        console.log(weather);
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    })
+            })
+        })
+
+        .catch(error => {
+            console.log(error);
+        })
+
+}
+// on form submit, call the function to obtain search data
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const city = userSearch.value;
     cityLocation(city);
+})
+
+button.addEventListener('click', () => {
+    const city = userSearch.value;
+    cityLocation(city);
+
 })
